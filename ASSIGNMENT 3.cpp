a43359e7b2b6fb5c01055ea711a6887b1235516e
information2020 <afrienduned4real@gmail.com>
2015-05-22 09:18:15 +0100

    c++ assignment
    program source code

---
+++ ASSIGNMENT 3.cpp
@@ -0,0 +1,26 @@
+/*
+* Question3.cpp
+*
+*	Name:			[IZUEGBU CHUKA KINGSLEY]
+*	Department:		[STATISTICS]
+*	Jamb Reg No:	[45309570AJ]
+*
+* 	Program that prints a box, an oval, an arrow and a diamond.
+*
+*	Stub file to enable me complete assignment #1 - question #3
+*/
+
+# include <iostream>
+main ()
+{
+	std::cout<<"********         ***                *               *       \n";
+	std::cout<<"*      *       *     *	           ***            *   *   \n";
+	std::cout<<"*      *      *       *	          *****          *     * \n";  
+	std::cout<<"*      *      *       *	            *           *       *\n";
+	std::cout<<"*      *      *       *             *          *         *\n";
+	std::cout<<"*      *      *       *             *           *       *\n";
+	std::cout<<"*      *      *       *             *            *     *\n";
+	std::cout<<"*      *       *     *              *             *   *  \n";
+	std::cout<<"********         ***                *               *  \n";
+	return 0;
+	}
